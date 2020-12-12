824383d7a07dabcc663147fd100718005b7413fa
krissuess <justkristy@yandex.ru>
2020-12-13 01:46:31 +0300

    2 if then added

--- src/test/java/qa/tests/GroupCreationTests.java
+++ src/test/java/qa/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("n","h","f"));
+        app.getGroupHelper().fillGroupForm(new GroupData("n",null,null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
 
