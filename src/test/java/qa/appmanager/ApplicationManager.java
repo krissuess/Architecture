71b98d028e98c18fa2b5e8b1e122953bc9c791e7
krissuess <justkristy@yandex.ru>
2020-12-13 00:50:57 +0300

    start test in diff browsers

--- src/test/java/qa/appmanager/ApplicationManager.java
+++ src/test/java/qa/appmanager/ApplicationManager.java
@@ -4,6 +4,9 @@ package qa.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +18,20 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/");
         groupHelper = new GroupHelper(wd);
--- src/test/java/qa/tests/TestBase.java
+++ src/test/java/qa/tests/TestBase.java
@@ -1,12 +1,13 @@
 package qa.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import qa.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    ApplicationManager app = new ApplicationManager();
+    ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
 
     @BeforeMethod(alwaysRun = true)
