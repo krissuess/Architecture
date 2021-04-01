b933debb56349c7bf14cbcc0572c3baa3c3770c7
krissuess <justkristy@yandex.ru>
2021-04-02 00:32:58 +0300

    add test environment

--- addressbook-web-tests/src/test/java/qa/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/tests/TestBase.java
@@ -9,7 +9,8 @@ import qa.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",
+            BrowserType.CHROME));
 
 
     @BeforeSuite
